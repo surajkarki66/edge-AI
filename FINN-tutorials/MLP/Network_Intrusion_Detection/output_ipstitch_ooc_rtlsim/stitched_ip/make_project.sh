02b0a8ad9fd78234cbff88a7165409b32919f795
Suraj Karki <suraj.karki@uni-bielefeld.de>
2025-10-11 18:00:06 +0200

    add end2end verification

--- FINN-tutorials/MLP/Network_Intrusion_Detection/output_ipstitch_ooc_rtlsim/stitched_ip/make_project.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash 
-cd /tmp/finn_dev_surajkarki/vivado_stitch_proj_pfqmgpy9
-vivado -mode batch -source make_project.tcl
-cd /home/surajkarki/Documents/My_Workspace/Coding_Works/finn
--- README.md
+++
@@ -1,2 +0,0 @@
-# Edge AI
-This repository contains all the code that I have written during my learning of edge ai.
